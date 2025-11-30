300673e83ca88267eb8ecf597ecc99d940a56acf
ericdeveloper <ericspsantos@gmail.com>
2025-11-30 19:33:36 -0300

    Mudando a ordem dos índices na barra de pesquisa

--- src/configs/Indexes.ts
+++ src/configs/Indexes.ts
@@ -1,8 +1,8 @@
 const indexes = [
   { label: "Publications", name: process.env.INDEX_PUBLICATION || "" },
   { label: "People", name: process.env.INDEX_PERSON || "" },
-  { label: "Journals", name: process.env.INDEX_JOURNAL || "" },
   { label: "Organizations", name: process.env.INDEX_ORGUNIT || "" },
+  { label: "Journals", name: process.env.INDEX_JOURNAL || "" },
   { label: "Patents", name: process.env.INDEX_PATENT || "" },
   { label: "Programs", name: process.env.INDEX_PROGRAM || "" },
   { label: "Research groups", name: process.env.INDEX_GROUP || "" },
