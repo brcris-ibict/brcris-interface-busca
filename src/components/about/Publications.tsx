56e85f8d319da2c76fc377f9f216c13835804385
ericdeveloper <ericspsantos@gmail.com>
2025-12-14 21:22:05 -0300

    Padroniza placeholder e referências bibliográficas em ABNT

--- src/components/about/Publications.tsx
+++ src/components/about/Publications.tsx
@@ -7,35 +7,35 @@ const Publications = () => {
 
       <Publication
         authors="SOUZA, M.; et al."
-        title="<strong>BrCris: a catalyst for advancing open science and scientific data processing in Brazil</strong>"
+        title="BrCris: a catalyst for advancing open science and scientific data processing in Brazil"
         link="http://hdl.handle.net/11366/2551"
         journal="In: EUROCRIS, 16., 2024. Anais… 2024."
       />
 
       <Publication
         authors="CARVALHO SEGUNDO, W. L. R.; et al."
-        title="<strong>Inovação e conectividade: uma perspectiva sobre o projeto BrCris e suas tecnologias para tratamento de dados científicos</strong>"
+        title="Inovação e conectividade: uma perspectiva sobre o projeto BrCris e suas tecnologias para tratamento de dados científicos"
         link="https://www.brapci.inf.br/v/301399"
         journal="In: WORKSHOP DE INFORMAÇÃO, DADOS E TECNOLOGIA, 7., 2024. Anais… 2024."
       />
 
       <Publication
         authors="DIAS, T. M. R.; et al."
-        title="<strong>Análises métricas potencializadas: a contribuição da plataforma BrCris na integração de repositórios</strong>"
+        title="Análises métricas potencializadas: a contribuição da plataforma BrCris na integração de repositórios"
         link="https://www.brapci.inf.br/v/306005"
         journal="In: ENCONTRO BRASILEIRO DE BIBLIOMETRIA E CIENTOMETRIA, 9., 2024. Anais… 2024."
       />
 
       <Publication
         authors="DIAS, T. M. R.; et al."
-        title="<strong>Como os dados exportados pela plataforma BrCris impulsionam análises bibliométricas e cientométricas</strong>"
+        title="Como os dados exportados pela plataforma BrCris impulsionam análises bibliométricas e cientométricas"
         link="https://hdl.handle.net/20.500.11959/brapci/306003"
         journal="In: ENCONTRO BRASILEIRO DE BIBLIOMETRIA E CIENTOMETRIA, 9., 2024. Anais… 2024."
       />
 
       <Publication
         authors="DIAS, T. M. R.; GABRIEL JUNIOR, R. F.; SILVA, V. S.; SOUZA, M. G.; OLIVEIRA, A. F.; CARVALHO SEGUNDO, W. L. R."
-        title="<strong>Development and implementation of the BrCris and BrCris-T platform: integration of scientific and technological data in Brazil</strong>"
+        title="Development and implementation of the BrCris and BrCris-T platform: integration of scientific and technological data in Brazil"
         link="https://doi.org/10.1016/j.procs.2024.11.050"
         journal="Procedia Computer Science, v. 249, p. 66–77, 2024."
       />
@@ -44,21 +44,21 @@ const Publications = () => {
 
       <Publication
         authors="MENDES, W.; GRANDO, R. L.; DIAS, T. M. R.; JORGE, V. A.; CARVALHO SEGUNDO, W. L. R."
-        title="<strong>Estudo de caso da integração do Observatório Ciência, Tecnologia e Inovação em Saúde da Fiocruz ao sistema BrCris/Ibict</strong>"
+        title="Estudo de caso da integração do Observatório Ciência, Tecnologia e Inovação em Saúde da Fiocruz ao sistema BrCris/Ibict"
         link="http://www.revistacientifica.uem.mz/revista/index.php/lcs/article/view/246/185"
         journal="Revista Científica da UEM: Série Letras e Ciências Sociais, v. 4, n. 1, 2023."
       />
 
       <Publication
         authors="VIDAL, L. H. C.; et al."
-        title="<strong>BrCris: visualização de indicadores científicos</strong>"
+        title="BrCris: visualização de indicadores científicos"
         link="https://www.brapci.inf.br/v/257789"
         journal="In: ENCONTRO NACIONAL DE PESQUISA E PÓS-GRADUAÇÃO EM CIÊNCIA DA INFORMAÇÃO, 23., 2023. Anais… 2023."
       />
 
       <Publication
         authors="DIAS, T. M. R.; et al."
-        title="<strong>Plataforma BrCris: uma abordagem abrangente para a análise e compreensão do ecossistema de pesquisa científica no Brasil</strong>"
+        title="Plataforma BrCris: uma abordagem abrangente para a análise e compreensão do ecossistema de pesquisa científica no Brasil"
         link="https://www.brapci.inf.br/v/257772"
         journal="In: ENCONTRO NACIONAL DE PESQUISA E PÓS-GRADUAÇÃO EM CIÊNCIA DA INFORMAÇÃO, 23., 2023. Anais… 2023."
       />
@@ -67,42 +67,42 @@ const Publications = () => {
 
       <Publication
         authors="DIAS, T. M. R.; MENA-CHALCO, J. P.; CARVALHO SEGUNDO, W. L. R.; PINTO, A. L.; MOREIRA, T. H. J."
-        title="<strong>BrCris: plataforma para integração, análises e visualização de dados técnico-científicos</strong>"
+        title="BrCris: plataforma para integração, análises e visualização de dados técnico-científicos"
         link="https://ojs.uel.br/revistas/uel/index.php/informacao/article/view/47215/48825"
         journal="Informação & Informação, v. 27, n. 3, 2022."
       />
 
       <Publication
         authors="PINTO, A. L.; CARVALHO SEGUNDO, W. L. R.; DIAS, T. M. R.; SILVA, V. S.; GOMES, J. C.; QUONIAM, L."
-        title="<strong>Brazil developing current research information systems (BrCris) as data sources for studies of research</strong>"
+        title="Brazil developing current research information systems (BrCris) as data sources for studies of research"
         link="https://dialnet.unirioja.es/servlet/articulo?codigo=8634724"
         journal="Iberoamerican Journal of Science Measurement and Communication, v. 2, n. 1, 2022."
       />
 
       <Publication
         authors="SILVA, V. S.; MATAS, L.; MOREIRA, T.; CARVALHO SEGUNDO, W. L. R."
-        title="<strong>An ETL strategy for integrating the LA Referencia platform and VIVO for the Brazilian CRIS</strong>"
+        title="An ETL strategy for integrating the LA Referencia platform and VIVO for the Brazilian CRIS"
         link="https://doi.org/10.1016/j.procs.2022.10.182"
         journal="Procedia Computer Science, n. 211, p. 111–117, 2022."
       />
 
       <Publication
         authors="CARVALHO SEGUNDO, W. L. R.; et al."
-        title="<strong>A strategy for collection, integration, and processing of scientific data in BrCris context</strong>"
+        title="A strategy for collection, integration, and processing of scientific data in BrCris context"
         link="https://doi.org/10.47909/anis.978-9916-9760-3-6.117"
         journal="Advanced Notes in Information Science, v. 2, p. 215–222, 2022."
       />
 
       <Publication
         authors="DIAS, T. M. R.; et al."
-        title="<strong>O projeto BrCris: uma plataforma computacional para integração, visualização e prospecção de dados científicos</strong>"
+        title="O projeto BrCris: uma plataforma computacional para integração, visualização e prospecção de dados científicos"
         link="https://brapci.inf.br/v/199466"
         journal="In: ENCONTRO BRASILEIRO DE BIBLIOMETRIA E CIENTOMETRIA, 8., 2022. Anais… p. 390–397."
       />
 
       <Publication
         authors="DIAS, T. M. R.; et al."
-        title="<strong>Plataforma para agregação e análises de dados técnico-científicos</strong>"
+        title="Plataforma para agregação e análises de dados técnico-científicos"
         link="https://widat2022.ufes.br/wp-content/uploads/2023/04/artigos/PLATAFORMA%20PARA%20AGREGA%C3%87%C3%83O%20E%20AN%C3%81LISES%20DE%20DADOS%20T%C3%89CNICOS-CIENT%C3%8DFICOS.pdf"
         journal="In: WORKSHOP DE INFORMAÇÃO, DADOS E TECNOLOGIA, 5., 2022, Vitória. Anais… 2022."
       />
@@ -111,63 +111,63 @@ const Publications = () => {
 
       <Publication
         authors="CARVALHO SEGUNDO, W. L. R.; et al."
-        title="<strong>O BrCris como ferramenta de apoio à ciência aberta</strong>"
+        title="O BrCris como ferramenta de apoio à ciência aberta"
         link="https://zenodo.org/record/5576817"
         journal="In: CONFERÊNCIA LUSO-BRASILEIRA DE CIÊNCIA ABERTA, 12., 2021. Anais… p. 1–37."
       />
 
       <Publication
         authors="PINTO, A. L.; CARVALHO SEGUNDO, W. L. R.; QUONIAM, L.; DIAS, T. M. R."
-        title="<strong>The Brazilian current research information system: BrCris</strong>"
+        title="The Brazilian current research information system: BrCris"
         link="https://dialnet.unirioja.es/servlet/articulo?codigo=8411204"
         journal="In: CONGRESSO ISKO ESPANHA–PORTUGAL, 2021."
       />
 
       <Publication
         authors="SILVA, V. S.; et al."
-        title="<strong>Um modelo semântico baseado em ontologia para o CRIS brasileiro</strong>"
+        title="Um modelo semântico baseado em ontologia para o CRIS brasileiro"
         link="https://dialnet.unirioja.es/servlet/articulo?codigo=8411201"
         journal="In: 2021."
       />
 
       <Publication
         authors="DIAS, T. M. R.; et al."
-        title="<strong>A informação patentária no contexto do BrCris</strong>"
+        title="A informação patentária no contexto do BrCris"
         link="https://repositorio.unb.br/bitstream/10482/43602/1/LIVRO_MultiplosCenariosInformacao.pdf"
         journal="In: 2021."
       />
 
       <Publication
         authors="CARVALHO SEGUNDO, W. L. R.; RODRIGUES, T. M."
-        title="<strong>On building a tool for finding datasets based on a list of researchers or publications</strong>"
+        title="On building a tool for finding datasets based on a list of researchers or publications"
         link="https://zenodo.org/record/4506826"
         journal="In: INTERNATIONAL CONFERENCE ON OPEN REPOSITORIES, 16., 2021, Denver. Anais… 2021."
       />
 
       <Publication
         authors="PINTO, A. L.; et al."
-        title="<strong>BrCris como um sistema de recomendação científico-tecnológica</strong>"
+        title="BrCris como um sistema de recomendação científico-tecnológica"
         link="https://www.researchgate.net/publication/356640074"
         journal="In: ENCONTRO NACIONAL DE PESQUISA E PÓS-GRADUAÇÃO EM CIÊNCIA DA INFORMAÇÃO, 21., 2021. Anais… 2021."
       />
 
       <Publication
         authors="PINTO, A. L.; et al."
-        title="<strong>Métricas de avaliação sobre revistas científicas para uso no BrCris/IBICT</strong>"
+        title="Métricas de avaliação sobre revistas científicas para uso no BrCris/IBICT"
         link="https://www.researchgate.net/publication/354616911"
         journal="In: LATMETRICAS, Medellín, 2021."
       />
 
       <Publication
         authors="PINTO, A. L.; et al."
-        title="<strong>Identificación de especialistas basado en orientaciones y participación en tribunales de tesis</strong>"
+        title="Identificación de especialistas basado en orientaciones y participación en tribunales de tesis"
         link="https://www.researchgate.net/publication/354617081"
         journal="In: 2021."
       />
 
       <Publication
         authors="CARVALHO SEGUNDO, W. L. R.; et al."
-        title="<strong>Uma plataforma para o tratamento e integração de dados científicos</strong>"
+        title="Uma plataforma para o tratamento e integração de dados científicos"
         link="https://sol.sbc.org.br/index.php/latinoware/article/view/19913"
         journal="In: CONGRESSO LATINO-AMERICANO DE SOFTWARE LIVRE E TECNOLOGIAS ABERTAS, 18., 2021, Foz do Iguaçu. Anais… 2021."
       />
@@ -176,14 +176,14 @@ const Publications = () => {
 
       <Publication
         authors="PINTO, A. L.; MAGELA, T.; DIAS, R.; SILVA, V. S."
-        title="<strong>Utilizando o framework LattesDataXplorer para vincular automaticamente os currículos da Plataforma Lattes à BDTD</strong>"
+        title="Utilizando o framework LattesDataXplorer para vincular automaticamente os currículos da Plataforma Lattes à BDTD"
         link="https://brapci.inf.br/v/136536"
         journal="Ciência da Informação, v. 48, n. 3, 2019."
       />
 
       <Publication
         authors="MATAS, L. J.; DIAS, T. M. R.; CARVALHO SEGUNDO, W. L. R."
-        title="<strong>Improving LA Referencia metadata by linking research profiles to repositories</strong>"
+        title="Improving LA Referencia metadata by linking research profiles to repositories"
         link="https://ridi.ibict.br/bitstream/123456789/1025/1/PaperLTW.pdf"
         journal="In: OPEN REPOSITORIES, 2019, Hamburg. Anais… 2019."
       />
