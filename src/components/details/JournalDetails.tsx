156a18f79ba71753f9cd5a8c63fd7d1efdd46cbc
ericdeveloper <ericspsantos@gmail.com>
2026-05-06 13:21:46 -0300

    refactor: padroniza JournalDetails e OrganizationDetails

--- src/components/details/JournalDetails.tsx
+++ src/components/details/JournalDetails.tsx
@@ -1,9 +1,10 @@
-import { ErrorBoundary, useSearch } from "@elastic/react-search-ui";
+import { useSearch } from "@elastic/react-search-ui";
 import Head from "next/head";
 import { useTranslation } from "next-i18next";
 import { CSVLink } from "react-csv";
 import { formatBooleanString } from "../../../utils/Utils";
 import { usePublicationYears } from "../../hooks/usePublicationYears";
+import NotFound from "../../pages/404";
 import CopyLink from "../CopyLink";
 import ShowAuthorItem from "../customResultView/ShowAuthorItem";
 import ShowItem from "../customResultView/ShowItem";
@@ -18,7 +19,7 @@ const journalCsvHeaders = [
   { label: "Ano", key: "year" },
 ];
 export default function JournalDetails() {
-  const { wasSearched, isLoading, results } = useSearch();
+  const { isLoading, results } = useSearch();
   const { t } = useTranslation("common");
 
   const result = results?.[0];
@@ -54,181 +55,159 @@ export default function JournalDetails() {
     id: p?.id ?? "",
     year: p?.year ?? "",
   }));
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (!isLoading && !result) {
+    return <NotFound />;
+  }
   return (
-    <div className="">
-      {isLoading && <Loader />}
-      <ErrorBoundary>
-        {wasSearched &&
-          results &&
-          results.length > 0 &&
-          results.map((result) => (
-            <div key={result.id}>
-              <Head>
-                <title>{`${result.title?.raw} | BrCris`}</title>
-              </Head>
-              <div className="mb-3 position-relative">
-                <div className="d-flex justify-content-between align-items-center">
-                  <h1 className="title mb-0">{result.title?.raw}</h1>
-                  <PopoverButton />
-                </div>
+    <div key={result.id}>
+      <Head>
+        <title>{`${result.title?.raw} | BrCris`}</title>
+      </Head>
+      <div className="mb-3 position-relative">
+        <div className="d-flex justify-content-between align-items-center">
+          <h1 className="title mb-0">{result.title?.raw}</h1>
+          <PopoverButton />
+        </div>
 
-                <div className="mt-2">
-                  {result.id?.raw && (
-                    <div className="d-flex align-items-center gap-2">
-                      <img
-                        className="brcris-logo"
-                        src="/logos/brcris-grafo.svg"
-                        alt="logo do BrCris"
-                      />
-                      <CopyLink
-                        link={`${location.origin}/journals/${result.id.raw}`}
-                      />
-                      <ReportPopoverButton />
-                    </div>
-                  )}
-                </div>
-              </div>
-              <div className="details-card">
-                <ul>
-                  <ShowItem label={t("Type")} value={result.type?.raw} />
-                  {result.researchArea?.raw?.length > 0 && (
-                    <li>
-                      <span className="sui-result__key">
-                        {t("Has subject area")}
-                      </span>
-                      <ExpandableContent
-                        items={result.researchArea?.raw}
-                        initialCount={5}
-                        renderItem={(area: any, idx: number) => (
-                          <div key={idx} className="research-area-item">
-                            {area?.name}
-                          </div>
-                        )}
-                      />
-                    </li>
-                  )}
-                  <ShowAuthorItem
-                    label={t("Publisher")}
-                    authors={result.publisher?.raw}
-                  />
-                  <ShowItem
-                    label={t("Is open access")}
-                    value={formatBooleanString(result.isOA?.raw[0], t)}
-                  />
-                  <ShowItem
-                    label={t("Is in DOAJ")}
-                    value={formatBooleanString(result.isInDoaj?.raw[0], t)}
-                  />
-                  <ShowItem
-                    label={t("2 year mean citedness")}
-                    value={result.googleH5?.raw}
-                  />
-                  <ShowItem label={t("H index")} value={result.h_index?.raw} />
-                  <ShowItem
-                    label={t("I10 index")}
-                    value={result.i10_index?.raw}
-                  />
-                  <ShowItem label={t("ISSN-L")} value={result.issn_l?.raw} />
-                  {result.issn?.raw?.length > 0 && (
-                    <li>
-                      <span className="sui-result__key">
-                        {t("International Standard Serial Number (ISSN)")}
-                      </span>
-                      <span>
-                        <ExpandableContent
-                          items={
-                            Array.isArray(result.issn.raw)
-                              ? result.issn.raw
-                              : [result.issn.raw]
-                          }
-                          initialCount={5}
-                          renderItem={(issn: string, idx: number) => {
-                            const url = `https://portal.issn.org/resource/ISSN/${issn}`;
-                            return (
-                              <div key={idx}>
-                                <a
-                                  href={url}
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                  className="break-url"
-                                >
-                                  {url}
-                                </a>
-                              </div>
-                            );
-                          }}
-                        />
-                      </span>
-                    </li>
-                  )}
-
-                  <ShowItem
-                    label={t("Qualis classification")}
-                    value={result.qualis?.raw}
-                  />
-                  {publications.length > 0 && (
-                    <li>
-                      <div className="d-flex justify-content-between align-items-center mb-2">
-                        <strong className="research-title">
-                          {t("Publications")}: ({publications.length})
-                        </strong>
-                        {/* @ts-ignore */}
-
-                        <CSVLink
-                          data={formattedPublicationsForCsv}
-                          headers={journalCsvHeaders}
-                          filename={`publicacoes-${result.title?.raw ?? "journal"}.csv`}
-                          className="btn btn-primary btn-sm"
-                        >
-                          ⬇ {t("Export csv")}
-                        </CSVLink>
-                      </div>
-
-                      <ExpandableContent
-                        items={sortedPublications}
-                        initialCount={5}
-                        renderItem={(publication: any) => (
-                          <div className="publication-item">
-                            <a href={`/publications/${publication?.id}`}>
-                              {publication?.title}
-                            </a>
-
-                            {getYearById(publication?.id) && (
-                              <div className="publication-meta">
-                                {getYearById(publication?.id)}
-                              </div>
-                            )}
-                          </div>
-                        )}
-                      />
-                    </li>
-                  )}
-                  <ShowItem
-                    label={t("Access type")}
-                    value={result.accessType?.raw}
-                  />
-                  <ShowItem label={t("Status")} value={result.status?.raw} />
-                  <ShowItem
-                    label={t("Language")}
-                    value={result.language?.raw}
-                  />
-                  <ShowItem
-                    label={t("Country code")}
-                    value={result.countryCode?.raw}
-                  />
-                  <ShowItem
-                    label={t("Assessment area")}
-                    value={result.assessmentArea?.raw}
-                  />
-                  <ShowItem
-                    label={t("Keywords")}
-                    value={result.keywords?.raw}
-                  />
-                </ul>
-              </div>
+        <div className="mt-2">
+          {result.id?.raw && (
+            <div className="d-flex align-items-center gap-2">
+              <img
+                className="brcris-logo"
+                src="/logos/brcris-grafo.svg"
+                alt="logo do BrCris"
+              />
+              <CopyLink link={`${location.origin}/journals/${result.id.raw}`} />
+              <ReportPopoverButton />
             </div>
-          ))}
-      </ErrorBoundary>
+          )}
+        </div>
+      </div>
+      <div className="details-card">
+        <ul>
+          <ShowItem label={t("Type")} value={result.type?.raw} />
+          {result.researchArea?.raw?.length > 0 && (
+            <li>
+              <span className="sui-result__key">{t("Has subject area")}</span>
+              <ExpandableContent
+                items={result.researchArea?.raw}
+                initialCount={5}
+                renderItem={(area: any, idx: number) => (
+                  <div key={idx} className="research-area-item">
+                    {area?.name}
+                  </div>
+                )}
+              />
+            </li>
+          )}
+          <ShowAuthorItem
+            label={t("Publisher")}
+            authors={result.publisher?.raw}
+          />
+          <ShowItem
+            label={t("Is open access")}
+            value={formatBooleanString(result.isOA?.raw[0], t)}
+          />
+          <ShowItem
+            label={t("Is in DOAJ")}
+            value={formatBooleanString(result.isInDoaj?.raw[0], t)}
+          />
+          <ShowItem
+            label={t("2 year mean citedness")}
+            value={result.googleH5?.raw}
+          />
+          <ShowItem label={t("H index")} value={result.h_index?.raw} />
+          <ShowItem label={t("I10 index")} value={result.i10_index?.raw} />
+          <ShowItem label={t("ISSN-L")} value={result.issn_l?.raw} />
+          {result.issn?.raw?.length > 0 && (
+            <li>
+              <span className="sui-result__key">
+                {t("International Standard Serial Number (ISSN)")}
+              </span>
+              <span>
+                <ExpandableContent
+                  items={
+                    Array.isArray(result.issn.raw)
+                      ? result.issn.raw
+                      : [result.issn.raw]
+                  }
+                  initialCount={5}
+                  renderItem={(issn: string, idx: number) => {
+                    const url = `https://portal.issn.org/resource/ISSN/${issn}`;
+                    return (
+                      <div key={idx}>
+                        <a
+                          href={url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="break-url"
+                        >
+                          {url}
+                        </a>
+                      </div>
+                    );
+                  }}
+                />
+              </span>
+            </li>
+          )}
+
+          <ShowItem
+            label={t("Qualis classification")}
+            value={result.qualis?.raw}
+          />
+          {publications.length > 0 && (
+            <li>
+              <div className="d-flex justify-content-between align-items-center mb-2">
+                <strong className="research-title">
+                  {t("Publications")}: ({publications.length})
+                </strong>
+                {/* @ts-ignore */}
+
+                <CSVLink
+                  data={formattedPublicationsForCsv}
+                  headers={journalCsvHeaders}
+                  filename={`publicacoes-${result.title?.raw ?? "journal"}.csv`}
+                  className="btn btn-primary btn-sm"
+                >
+                  ⬇ {t("Export csv")}
+                </CSVLink>
+              </div>
+
+              <ExpandableContent
+                items={sortedPublications}
+                initialCount={5}
+                renderItem={(publication: any) => (
+                  <div className="publication-item">
+                    <a href={`/publications/${publication?.id}`}>
+                      {publication?.title}
+                    </a>
+
+                    {getYearById(publication?.id) && (
+                      <div className="publication-meta">
+                        {getYearById(publication?.id)}
+                      </div>
+                    )}
+                  </div>
+                )}
+              />
+            </li>
+          )}
+          <ShowItem label={t("Access type")} value={result.accessType?.raw} />
+          <ShowItem label={t("Status")} value={result.status?.raw} />
+          <ShowItem label={t("Language")} value={result.language?.raw} />
+          <ShowItem label={t("Country code")} value={result.countryCode?.raw} />
+          <ShowItem
+            label={t("Assessment area")}
+            value={result.assessmentArea?.raw}
+          />
+          <ShowItem label={t("Keywords")} value={result.keywords?.raw} />
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/details/OrganizationDetails.tsx
+++ src/components/details/OrganizationDetails.tsx
@@ -4,11 +4,11 @@ import { useTranslation } from "next-i18next";
 import { CSVLink } from "react-csv";
 import { getLattesIdentifier } from "../../../utils/Utils";
 import { usePersonIdentifiers } from "../../hooks/usePersonIdentifiers";
+import NotFound from "../../pages/404";
 import CopyLink from "../CopyLink";
 import ShowItem from "../customResultView/ShowItem";
 import ExpandableContent from "../ExpandableContent";
 import Loader from "../Loader";
-import PopoverButton from "../PopOver";
 import ReportPopoverButton from "../ReportPopoverButton";
 
 const membersCsvHeaders = [
@@ -18,188 +18,156 @@ const membersCsvHeaders = [
 ];
 
 export default function OrganizationDetails() {
-  const { wasSearched, isLoading, results } = useSearch();
+  const { isLoading, results } = useSearch();
   const { t } = useTranslation("common");
-  const memberIds =
-    results?.flatMap((r: any) => r.member?.raw?.map((m: any) => m.id) ?? []) ??
-    [];
+  const result = results?.[0];
 
+  const members = result?.member?.raw || [];
+  const memberIds = members.map((m: any) => m.id);
   const { data: personData } = usePersonIdentifiers(memberIds);
-
   const personMap = new Map((personData || []).map((p: any) => [p.id, p]));
+  const membersCsvData = members.map((member: any) => {
+    const extra = personMap.get(member.id);
+
+    return {
+      name: member?.name ?? "",
+      lattesId: getLattesIdentifier(extra?.lattesId),
+      brcrisId: extra?.brcrisId ?? member?.id ?? "",
+    };
+  });
+  if (isLoading) return <Loader />;
+  if (!result) return <NotFound />;
   return (
-    <div className="">
-      {isLoading && <Loader />}
-      <ErrorBoundary>
-        {wasSearched &&
-          results &&
-          results.length > 0 &&
-          results.map((result) => {
-            const membersCsvData =
-              result.member?.raw?.map((member: any) => {
-                const extra = personMap.get(member.id);
+    <div key={result.id}>
+      <Head>
+        <title>{`${result.name?.raw} | BrCris`}</title>
+      </Head>
+      <div className="mb-3 position-relative">
+        <div className="d-flex justify-content-between align-items-center">
+          <h1 className="title mb-0">{result.name?.raw}</h1>
+        </div>
 
-                return {
-                  name: member?.name ?? "",
-                  lattesId: getLattesIdentifier(extra?.lattesId),
-                  brcrisId: extra?.brcrisId ?? member?.id ?? "",
-                };
-              }) ?? [];
+        <div className="mt-2">
+          {result.id?.raw && (
+            <div className="d-flex align-items-center gap-2">
+              <img
+                className="brcris-logo"
+                src="/logos/brcris-grafo.svg"
+                alt="logo do BrCris"
+              />
+              <CopyLink
+                link={`${location.origin}/organizations/${result.id.raw}`}
+              />
+              <ReportPopoverButton />
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="details-card">
+        <ul>
+          {result.member?.raw?.length > 0 && (
+            <li>
+              <div className="d-flex justify-content-between align-items-center">
+                <span className="sui-result__key">{t("Has member")}</span>
+                {/* @ts-ignore */}
 
-            return (
-              <div key={result.id}>
-                <Head>
-                  <title>{`${result.name?.raw} | BrCris`}</title>
-                </Head>
-                <div className="mb-3 position-relative">
-                  <div className="d-flex justify-content-between align-items-center">
-                    <h1 className="title mb-0">{result.name?.raw}</h1>
+                <CSVLink
+                  data={membersCsvData}
+                  headers={membersCsvHeaders}
+                  filename={`membros-${result.name?.raw ?? "organizacao"}.csv`}
+                  className="btn btn-primary btn-sm"
+                >
+                  ⬇ {t("Export csv")}
+                </CSVLink>
+              </div>{" "}
+              <ExpandableContent
+                items={[...result.member.raw].sort((a: any, b: any) =>
+                  String(a?.name?.raw ?? a?.name ?? "").localeCompare(
+                    String(b?.name?.raw ?? b?.name ?? ""),
+                  ),
+                )}
+                initialCount={5}
+                renderItem={(item: any, idx: number) => (
+                  <div key={idx} className="member-item">
+                    <a href={`/people/${item.id}`}>{item?.name}</a>
                   </div>
+                )}
+              />
+            </li>
+          )}
+          <ShowItem value={result.acronym?.raw} label={t("Acronym")} />
+          <ShowItem value={result.country?.raw} label={t("Country")} />
+          <ShowItem value={result.state?.raw} label={t("State")} />
+          <ShowItem value={result.city?.raw} label={t("City")} />
 
-                  <div className="mt-2">
-                    {result.id?.raw && (
-                      <div className="d-flex align-items-center gap-2">
-                        <img
-                          className="brcris-logo"
-                          src="/logos/brcris-grafo.jpeg"
-                          alt="logo do BrCris"
-                        />
-                        <CopyLink
-                          link={`${location.origin}/organizations/${result.id.raw}`}
-                        />
-                        <ReportPopoverButton />
-                      </div>
-                    )}
+          {result.capesId?.raw?.length > 0 && (
+            <li>
+              <span className="identifier-key">{t("Capes identifier")}:</span>
+              <span>
+                <ExpandableContent
+                  items={result.capesId.raw}
+                  initialCount={5}
+                  renderItem={(item: string) => <>{item}</>}
+                />
+              </span>
+            </li>
+          )}
+          {result.program?.raw?.length > 0 && (
+            <li>
+              <span className="sui-result__key">{t("Program")}</span>
+              <span>
+                <ExpandableContent
+                  items={[...result.program.raw].sort((a: any, b: any) =>
+                    String(a?.name?.raw ?? a?.name ?? "").localeCompare(
+                      String(b?.name?.raw ?? b?.name ?? ""),
+                    ),
+                  )}
+                  initialCount={5}
+                  renderItem={(program: any) => (
+                    <>
+                      {program.name && (
+                        <a href={`/programs/${program.id}`}>{program.name}</a>
+                      )}
+                    </>
+                  )}
+                />
+              </span>
+            </li>
+          )}
+          {result.course?.raw?.length > 0 && (
+            <li>
+              <span className="sui-result__key">{t("Course")}</span>
+              <span>
+                <ExpandableContent
+                  items={result.course.raw}
+                  initialCount={5}
+                  renderItem={(item: any) => (
+                    <>
+                      <a href={`/organizations/${item.id}`}>{item?.name}</a>
+                    </>
+                  )}
+                />
+              </span>
+            </li>
+          )}
+          {result.publication?.raw?.length > 0 && (
+            <li>
+              <span className="sui-result__key">{t("Publications")}</span>
+              <ExpandableContent
+                items={result.publication?.raw}
+                initialCount={5}
+                renderItem={(publication: any, index: number) => (
+                  <div key={index} className="publication-item">
+                    <a href={`/publications/${publication?.id}`}>
+                      {publication?.title}
+                    </a>
                   </div>
-                </div>
-                <div className="details-card">
-                  <ul>
-                    {result.member?.raw?.length > 0 && (
-                      <li>
-                        <div className="d-flex justify-content-between align-items-center">
-                          <span className="sui-result__key">
-                            {t("Has member")}
-                          </span>
-                          {/* @ts-ignore */}
-
-                          <CSVLink
-                            data={membersCsvData}
-                            headers={membersCsvHeaders}
-                            filename={`membros-${result.name?.raw ?? "organizacao"}.csv`}
-                            className="btn btn-primary btn-sm"
-                          >
-                            ⬇ {t("Export csv")}
-                          </CSVLink>
-                        </div>{" "}
-                        <ExpandableContent
-                          items={[...result.member.raw].sort((a: any, b: any) =>
-                            String(a?.name?.raw ?? a?.name ?? "").localeCompare(
-                              String(b?.name?.raw ?? b?.name ?? ""),
-                            ),
-                          )}
-                          initialCount={5}
-                          renderItem={(item: any, idx: number) => (
-                            <div key={idx} className="member-item">
-                              <a href={`/people/${item.id}`}>{item?.name}</a>
-                            </div>
-                          )}
-                        />
-                      </li>
-                    )}
-                    <ShowItem
-                      value={result.acronym?.raw}
-                      label={t("Acronym")}
-                    />
-                    <ShowItem
-                      value={result.country?.raw}
-                      label={t("Country")}
-                    />
-                    <ShowItem value={result.state?.raw} label={t("State")} />
-                    <ShowItem value={result.city?.raw} label={t("City")} />
-
-                    {result.capesId?.raw?.length > 0 && (
-                      <li>
-                        <span className="identifier-key">
-                          {t("Capes identifier")}:
-                        </span>
-                        <span>
-                          <ExpandableContent
-                            items={result.capesId.raw}
-                            initialCount={5}
-                            renderItem={(item: string) => <>{item}</>}
-                          />
-                        </span>
-                      </li>
-                    )}
-                    {result.program?.raw?.length > 0 && (
-                      <li>
-                        <span className="sui-result__key">{t("Program")}</span>
-                        <span>
-                          <ExpandableContent
-                            items={[...result.program.raw].sort(
-                              (a: any, b: any) =>
-                                String(
-                                  a?.name?.raw ?? a?.name ?? "",
-                                ).localeCompare(
-                                  String(b?.name?.raw ?? b?.name ?? ""),
-                                ),
-                            )}
-                            initialCount={5}
-                            renderItem={(program: any) => (
-                              <>
-                                {program.name && (
-                                  <a href={`/programs/${program.id}`}>
-                                    {program.name}
-                                  </a>
-                                )}
-                              </>
-                            )}
-                          />
-                        </span>
-                      </li>
-                    )}
-                    {result.course?.raw?.length > 0 && (
-                      <li>
-                        <span className="sui-result__key">{t("Course")}</span>
-                        <span>
-                          <ExpandableContent
-                            items={result.course.raw}
-                            initialCount={5}
-                            renderItem={(item: any) => (
-                              <>
-                                <a href={`/organizations/${item.id}`}>
-                                  {item?.name}
-                                </a>
-                              </>
-                            )}
-                          />
-                        </span>
-                      </li>
-                    )}
-                    {result.publication?.raw?.length > 0 && (
-                      <li>
-                        <span className="sui-result__key">
-                          {t("Publications")}
-                        </span>
-                        <ExpandableContent
-                          items={result.publication?.raw}
-                          initialCount={5}
-                          renderItem={(publication: any, index: number) => (
-                            <div key={index} className="publication-item">
-                              <a href={`/publications/${publication?.id}`}>
-                                {publication?.title}
-                              </a>
-                            </div>
-                          )}
-                        />
-                      </li>
-                    )}
-                  </ul>
-                </div>
-              </div>
-            );
-          })}
-      </ErrorBoundary>
+                )}
+              />
+            </li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
